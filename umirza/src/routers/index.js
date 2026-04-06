4e2119a6072c29186a758ba7a6baed8e41164b95
Ubeyd Mirza Duman <mrzdmn@hotmail.com>
2026-04-07 01:46:14 +0300

    Update index.js

--- umirza/src/routers/index.js
+++ umirza/src/routers/index.js
@@ -1,7 +1,7 @@
 import { createRouter,createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Login from "../views/Login.vue";
-import { Component } from "react";
+
 
 const routes=[
     {
